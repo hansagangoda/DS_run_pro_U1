f52b80d2a7f53874b5272c80af92b9e7b30a22ff
hansagangoda <vishalgangoda825@gmail.com>
2024-09-02 15:24:59 +0530

    Add files via upload

--- stack2.c
+++ stack2.c
@@ -1,143 +1,119 @@
-//program 2 implement opretions of stack push,pop,peep,modify,display
-#include<stdio.h>
-#include<conio.h>
-#define MAX 3
+//pro2 stack
+#include <stdio.h>
+#include <stdlib.h>
 
-int a[MAX], top = -1;
-void push();
-void pop();
-void peep();
-void change();
-void display();
+#define MAX 10
 
-void main()
+
+void push(int stack[], int *top, int value);
+void pop(int stack[], int *top);
+void peep(int stack[], int top);
+void modify(int stack[], int top, int index, int newValue);
+void display(int stack[], int top);
+
+int main() {
+    int stack[MAX];
+    int top = -1; 
+    int choice, value, index;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peep\n");
+        printf("4. Modify\n");
+        printf("5. Display\n");
+        printf("0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                if (top < MAX - 1) {
+                    printf("Enter value to push: ");
+                    scanf("%d", &value);
+                    push(stack, &top, value);
+                } else {
+                    printf("Stack is full. Cannot push more elements.\n");
+                }
+                break;
+            case 2:
+                pop(stack, &top);
+                break;
+            case 3:
+                peep(stack, top);
+                break;
+            case 4:
+                if (top >= 0) {
+                    printf("Enter index to modify (0 to %d): ", top);
+                    scanf("%d", &index);
+                    if (index >= 0 && index <= top) {
+                        printf("Enter new value: ");
+                        scanf("%d", &value);
+                        modify(stack, top, index, value);
+                    } else {
+                        printf("Invalid index.\n");
+                    }
+                } else {
+                    printf("Stack is empty. No elements to modify.\n");
+                }
+                break;
+            case 5:
+                display(stack, top);
+                break;
+            case 0:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 0);
+
+    return 0;
+}
+
+
+void push(int stack[], int *top, int value) {
+    stack[++(*top)] = value;
+    printf("Pushed %d onto stack.\n", value);
+}
+
+
+void pop(int stack[], int *top) {
+    if (*top >= 0) {
+        printf("Popped %d from stack.\n", stack[(*top)--]);
+    } else {
+        printf("Stack is empty. Nothing to pop.\n");
+    }
+}
+
+
+void peep(int stack[], int top) {
+    if (top >= 0) {
+        printf("Top element is %d.\n", stack[top]);
+    } else {
+        printf("Stack is empty.\n");
+    }
+}
+
+
+void modify(int stack[], int top, int index, int newValue) {
+    stack[index] = newValue;
+    printf("Modified index %d to new value %d.\n", index, newValue);
+}
+
+void display(int stack[], int top)
 {
-  int ch;
-  while(1) {
-     printf("\n1. PUSH or INSERT");
-  printf("\n2. POP or DELETE");
-  printf("\n3. PEEP or SEARCH");
-  printf("\n4. CHANGE or UPDATE");
-  printf("\n5. Display");
-  printf("\n6. End program");
-     printf("\n opretion stack Enter Choice : ");
-     scanf("%d",&ch);
-     
-     switch(ch)
-     {
-       case 1:
- {
-    push();
-    break;
- }
-       case 2:
- {
-    pop();
-    break;
- }
- case 3:
-       {
-   peep();
-   break;
-       }
-       case 4:
-       {
-   change();
-   break;
-       }
-       case 5:
- {
-    display();
-    break;
- }
-       case 6:
- {
-    exit(0);
- }
-       default:
- printf("\ninvalid choice !!!");
-     } // switch close
-     getch();
-  }
-  //getch();
+	int i;
+    if (top >= 0) {
+        printf("Stack elements:\n");
+        for (i = 0; i <= top; i++)
+		{
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    } else {
+        printf("Stack is empty.\n");
+    }
 }
-
-void push(){
-  int data;
-  if(top==MAX-1)
-  {
-     printf("\noverflow or stack is full !!!");
-  }
-  else
-  {
-     printf("\nEnter the element : ");
-     scanf("%d",&data);
-     top++;
-     a[top]=data;
-  }
-}
-
-void pop()
-{
-  if(top==-1)
-  {
-    printf("\nunder flow STACK or STACK is empty");
-  }
-
-  else
-  {
-    printf("\nPOP or DELETE element :  %d",a[top]);
-    top--;
-  }
-}
-
-void display()
-{
-   int i;
-   if(top>=0)
-   {
-       printf("\nElemets : ");
-       for(i=top; i>=0; i--)
-       {
-  printf("\n%d",a[i]);
-       }
-   }
-   else
-   {
-       printf("\nThe STACK is Empty");
-   }
-}
-void peep()
-{
-   int p;
-   printf("\nEnter the position : ");
-   scanf("%d",&p);
-   if(top-p<=-1)
-   {
-      printf("\nSTACK is overflow !!!");
-   }
-   else
-   {
-      printf("\nThe Elements is : %d",a[top-p]);
-   }
-}
-void change()
-{
-  int v1,v2;
-  printf("\nEnter Position for change : ");
-  scanf("%d",&v1);
-  printf("\nEneter the Number for change : ");
-  scanf("%d",&v2);
-  if(top-v1<=-1)
-  {
-     printf("\nSTACK is overflow !!!");
-  }
-  else
-  {
-    a[top-v1]=v2;
-    printf("\nCHANGE successfull !!!");
-  }
-}
-
-        
 
